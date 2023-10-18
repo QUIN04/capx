ae661fbc00af4515f4668429f8fd38d86413109b
QUIN04 <tquinuel@gmail.com>
2023-10-18 17:02:38 +0100

    Assist Capacity Exchange Development - Task 4

--- models.py
+++ models.py
@@ -3,6 +3,17 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+
+from django.contrib.auth.models import AbstractUser
+
+from django.utils.html import escape, mark_safe
+
+
 
 class Bug(models.Model):
     description = models.CharField(max_length=300)
